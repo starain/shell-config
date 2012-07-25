e8983b0f0021ffe101872e83c99df2e97714660b
Yi Zhang <starain.zhang@gmail.com>
2012-07-24 17:42:10 -0700

    Try to use git clone for files

--- sbin/setup_local_home.sh
+++ sbin/setup_local_home.sh
@@ -1,41 +1,55 @@
 #!/bin/bash
 
 # Setup environment
-CONFIG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+if [ $# -lt 1 ]
+then
+  echo "Usage ${BASH_SOURCE[0]} target_config_dir [environment_sub_dir]"
+fi
+
+SOURCE_CONFIG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
+CONFIG_DIR="$1"
 ENV_DIR="$CONFIG_DIR"
-if [ $# -eq 1 ]
+if [ $# -eq 2 ]
 then
     ENV_DIR="$CONFIG_DIR/$1"
 fi
 
-# Setup bashrc
-echo "# BEGIN: Setup by setup_local_home.sh" >> ~/.bashrc
-echo "export ZHANGYI_CONFIG_DIR=\"$CONFIG_DIR\"" >> ~/.bashrc
-echo "export ZHANGYI_ENV_DIR=\"$ENV_DIR\"" >> ~/.bashrc
+mkdir -p $CONFIG_DIR
+cd $CONFIG_DIR
 
-echo 'if [ -f "$ZHANGYI_ENV_DIR/bashrc/bashrc.sh" ]; then' >> ~/.bashrc
-echo '    . "$ZHANGYI_ENV_DIR/bashrc/bashrc.sh"' >> ~/.bashrc
-echo 'fi' >> ~/.bashrc
-echo "# END: Setup by setup_local_home.sh" >> ~/.bashrc
+for sub_dir in $(ls $SOURCE_CONFIG_DIR)
+do
+  git clone $SOURCE_CONFIG_DIR/$sub_dir
+done
+
+# Setup bashrc
+# echo "# BEGIN: Setup by setup_local_home.sh" >> ~/.bashrc
+# echo "export ZHANGYI_CONFIG_DIR=\"$CONFIG_DIR\"" >> ~/.bashrc
+# echo "export ZHANGYI_ENV_DIR=\"$ENV_DIR\"" >> ~/.bashrc
+
+# echo 'if [ -f "$ZHANGYI_ENV_DIR/shell/bashrc/bashrc.sh" ]; then' >> ~/.bashrc
+# echo '    . "$ZHANGYI_ENV_DIR/shell/bashrc/bashrc.sh"' >> ~/.bashrc
+# echo 'fi' >> ~/.bashrc
+# echo "# END: Setup by setup_local_home.sh" >> ~/.bashrc
 
 # Setup profile
-echo "# BEGIN: Setup by setup_local_home.sh" >> ~/.profile
-echo 'if [ -f "$ZHANGYI_ENV_DIR/profile.sh" ]; then' >> ~/.profile
-echo '    . "$ZHANGYI_ENV_DIR/profile.sh"' >> ~/.profile
-echo 'fi' >> ~/.profile
-echo "# END: Setup by setup_local_home.sh" >> ~/.profile
+# echo "# BEGIN: Setup by setup_local_home.sh" >> ~/.profile
+# echo 'if [ -f "$ZHANGYI_ENV_DIR/shell/profile.sh" ]; then' >> ~/.profile
+# echo '    . "$ZHANGYI_ENV_DIR/shell/profile.sh"' >> ~/.profile
+# echo 'fi' >> ~/.profile
+# echo "# END: Setup by setup_local_home.sh" >> ~/.profile
 
 # Setup emacs
-echo ";; BEGIN: Setup by setup_local_home.sh" >> ~/.emacs
-echo "(load-file \"$ENV_DIR/emacs/start.el\")" >> ~/.emacs
-echo ";; END: Setup by setup_local_home.sh" >> ~/.emacs
+# echo ";; BEGIN: Setup by setup_local_home.sh" >> ~/.emacs
+# echo "(load-file \"$ENV_DIR/emacs/start.el\")" >> ~/.emacs
+# echo ";; END: Setup by setup_local_home.sh" >> ~/.emacs
 
 # Setup screen
-if [ -f "$CONFIG_DIR/screenrc" ]; then
-    ln -sf "$CONFIG_DIR/screenrc" ~/.screenrc
-fi
+# if [ -f "$CONFIG_DIR/shell/screenrc" ]; then
+#     ln -sf "$CONFIG_DIR/shell/screenrc" ~/.screenrc
+# fi
 
 # Setup environment related stuff
-if [ -f "$ENV_DIR/sbin/setup_local_home.sh" ]; then
-    . "$ENV_DIR/sbin/setup_local_home.sh"
-fi
+# if [ -f "$ENV_DIR/sbin/setup_local_home.sh" ]; then
+#     . "$ENV_DIR/sbin/setup_local_home.sh"
+# fi
