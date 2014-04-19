e823e5c8b464fdc75a35b4634bebd7ed1b2818cd
Yi Zhang <starain.zhang@gmail.com>
2014-04-18 21:53:37 -0700

    update emacs

--- bashrc/alias.sh
+++ bashrc/alias.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-alias rs='screen -x yiz || screen -S yiz'
+alias rs='screen -h 5000 -x yiz || screen -h 5000 -S yiz'
 alias ls='ls --color=auto'
 alias l='ls'
-alias e='emacsclient -t'
+alias e='emacsclient -a ""'
 alias g='git'
 alias gcm='git commit -a -m'
 alias gl='git log'
--- bashrc/bashrc.sh
+++ bashrc/bashrc.sh
@@ -3,3 +3,6 @@
 if [ -f "$ZHANGYI_CONFIG_DIR/shell/bashrc/alias.sh" ]; then
     . "$ZHANGYI_CONFIG_DIR/shell/bashrc/alias.sh"
 fi
+
+export EDITOR='emacsclient -a ""'
+
