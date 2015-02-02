7bd629c76498de62fdcbaaa9b4a78a48f17d3d5b
Yi Zhang <starain.zhang@gmail.com>
2015-02-01 16:18:08 -0800

    fix couple of setup problems

--- bashrc/bashrc.sh
+++ bashrc/bashrc.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-if [ -f "$ZHANGYI_CONFIG_DIR/shell/zshrc/zshrc" ]; then
-    . "$ZHANGYI_CONFIG_DIR/shell/zshrc/zshrc"
-fi
-
-if [ -f "$ZHANGYI_CONFIG_DIR/shell/bashrc/alias.sh" ]; then
-    . "$ZHANGYI_CONFIG_DIR/shell/bashrc/alias.sh"
+if [ -f "$ZHANGYI_CONFIG_DIR/shell-config/bashrc/alias.sh" ]; then
+    . "$ZHANGYI_CONFIG_DIR/shell-config/bashrc/alias.sh"
 fi
 
 export EDITOR='emacsclient -a ""'
--- sbin/setup_local_home.sh
+++ sbin/setup_local_home.sh
@@ -3,17 +3,18 @@
 # Setup environment
 if [ $# -lt 1 ]
 then
-  echo "Usage ${BASH_SOURCE[0]} target_config_dir [environment_sub_dir]"
+    echo "Usage ${BASH_SOURCE[0]} target_config_dir [environment_sub_dir]"
+    exit
 fi
 
 SOURCE_CONFIG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 CONFIG_DIR="$1"
-ENV_DIR_NAME=""
+ENV_DIR="$CONFIG_DIR"
 if [ $# -eq 2 ]
 then
     ENV_DIR_NAME="$2"
+    ENV_DIR="$CONFIG_DIR/$ENV_DIR_NAME"
 fi
-ENV_DIR="$CONFIG_DIR/$ENV_DIR_NAME"
 
 if [ "$SOURCE_CONFIG_DIR" != "$CONFIG_DIR" ]
 then
@@ -36,15 +37,24 @@ echo "# BEGIN: Setup by setup_local_home.sh" >> ~/.bashrc
 echo "export ZHANGYI_CONFIG_DIR=\"$CONFIG_DIR\"" >> ~/.bashrc
 echo "export ZHANGYI_ENV_DIR=\"$ENV_DIR\"" >> ~/.bashrc
 
-echo 'if [ -f "$ZHANGYI_ENV_DIR/shell/bashrc/bashrc.sh" ]; then' >> ~/.bashrc
-echo '    . "$ZHANGYI_ENV_DIR/shell/bashrc/bashrc.sh"' >> ~/.bashrc
+echo 'if [ -f "$ZHANGYI_ENV_DIR/shell-config/bashrc/bashrc.sh" ]; then' >> ~/.bashrc
+echo '    . "$ZHANGYI_ENV_DIR/shell-config/bashrc/bashrc.sh"' >> ~/.bashrc
 echo 'fi' >> ~/.bashrc
 echo "# END: Setup by setup_local_home.sh" >> ~/.bashrc
 
+echo "# BEGIN: Setup by setup_local_home.sh" >> ~/.zshrc
+echo "export ZHANGYI_CONFIG_DIR=\"$CONFIG_DIR\"" >> ~/.zshrc
+echo "export ZHANGYI_ENV_DIR=\"$ENV_DIR\"" >> ~/.zshrc
+
+echo 'if [ -f "$ZHANGYI_ENV_DIR/shell-config/zshrc/zshrc" ]; then' >> ~/.zshrc
+echo '    . "$ZHANGYI_ENV_DIR/shell-config/zshrc/zshrc"' >> ~/.zshrc
+echo 'fi' >> ~/.zshrc
+echo "# END: Setup by setup_local_home.sh" >> ~/.zshrc
+
 # Setup profile
 echo "# BEGIN: Setup by setup_local_home.sh" >> ~/.profile
-echo 'if [ -f "$ZHANGYI_ENV_DIR/shell/profile.sh" ]; then' >> ~/.profile
-echo '    . "$ZHANGYI_ENV_DIR/shell/profile.sh"' >> ~/.profile
+echo 'if [ -f "$ZHANGYI_ENV_DIR/shell-config/profile.sh" ]; then' >> ~/.profile
+echo '    . "$ZHANGYI_ENV_DIR/shell-config/profile.sh"' >> ~/.profile
 echo 'fi' >> ~/.profile
 echo "# END: Setup by setup_local_home.sh" >> ~/.profile
 
@@ -54,16 +64,18 @@ echo "(load-file \"$ENV_DIR/emacs-config/start.el\")" >> ~/.emacs
 echo ";; END: Setup by setup_local_home.sh" >> ~/.emacs
 
 # Setup screen
-if [ -f "$CONFIG_DIR/shell/screenrc" ]; then
-    ln -sf "$CONFIG_DIR/shell/screenrc" ~/.screenrc
+if [ -f "$CONFIG_DIR/shell-config/screenrc" ]; then
+    ln -sf "$CONFIG_DIR/shell-config/screenrc" ~/.screenrc
 fi
 
 # Setup git
-if [ -f "$CONFIG_DIR/shell/gitignore" ]; then
-    ln -sf "$CONFIG_DIR/shell/gitignore" ~/.gitignore
+if [ -f "$CONFIG_DIR/shell-config/gitignore" ]; then
+    ln -sf "$CONFIG_DIR/shell-config/gitignore" ~/.gitignore
 fi
 
 # Setup environment related stuff
-if [ -f "$ENV_DIR/shell/sbin/setup_local_home.sh" ]; then
-    . "$ENV_DIR/shell/sbin/setup_local_home.sh"
+if [ "$ENV_DIR" != "$CONFIG_DIR" ]; then
+    if [ -f "$ENV_DIR/shell-config/sbin/setup_local_home.sh" ]; then
+        . "$ENV_DIR/shell-config/sbin/setup_local_home.sh"
+    fi
 fi
