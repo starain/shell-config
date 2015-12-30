aa0e6b4eccf7f8abdad477135554785022a9692a
Yi Zhang <starain.zhang@gmail.com>
2015-12-29 23:50:20 -0800

    initial reorg

--- sbin/setkeymap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-xmodmap -e "remove lock = Caps_Lock"
-xmodmap -e "add mod4 = Caps_Lock"
--- sbin/setup_local_home.sh
+++ sbin/setup_local_home.sh
@@ -7,8 +7,7 @@ then
     exit
 fi
 
-SOURCE_CONFIG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
-CONFIG_DIR="$SOURCE_CONFIG_DIR"
+CONFIG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 ENV_DIR="$CONFIG_DIR"
 if [ $# -eq 1 ]
 then
@@ -16,50 +15,57 @@ then
     ENV_DIR="$CONFIG_DIR/$ENV_DIR_NAME"
 fi
 
+MODULE_NAME="shell-config"
+
 # Setup bashrc
 echo "# BEGIN: Setup by setup_local_home.sh" >> ~/.bashrc
 echo "export ZHANGYI_CONFIG_DIR=\"$CONFIG_DIR\"" >> ~/.bashrc
 echo "export ZHANGYI_ENV_DIR=\"$ENV_DIR\"" >> ~/.bashrc
 
-echo 'if [ -f "$ZHANGYI_ENV_DIR/shell-config/bashrc/bashrc.sh" ]; then' >> ~/.bashrc
-echo '    . "$ZHANGYI_ENV_DIR/shell-config/bashrc/bashrc.sh"' >> ~/.bashrc
+echo 'if [ -f "$ZHANGYI_ENV_DIR/'"${MODULE_NAME}"'/bashrc/bashrc.sh" ]; then' >> ~/.bashrc
+echo '    . "$ZHANGYI_ENV_DIR/'"${MODULE_NAME}"'/bashrc/bashrc.sh"' >> ~/.bashrc
 echo 'fi' >> ~/.bashrc
 echo "# END: Setup by setup_local_home.sh" >> ~/.bashrc
 
+# Setup zshrc
 echo "# BEGIN: Setup by setup_local_home.sh" >> ~/.zshrc
 echo "export ZHANGYI_CONFIG_DIR=\"$CONFIG_DIR\"" >> ~/.zshrc
 echo "export ZHANGYI_ENV_DIR=\"$ENV_DIR\"" >> ~/.zshrc
 
-echo 'if [ -f "$ZHANGYI_ENV_DIR/shell-config/zshrc/zshrc" ]; then' >> ~/.zshrc
-echo '    . "$ZHANGYI_ENV_DIR/shell-config/zshrc/zshrc"' >> ~/.zshrc
+echo 'if [ -f "$ZHANGYI_ENV_DIR/'"${MODULE_NAME}"'/zshrc/zshrc" ]; then' >> ~/.zshrc
+echo '    . "$ZHANGYI_ENV_DIR/'"${MODULE_NAME}"'/zshrc/zshrc"' >> ~/.zshrc
 echo 'fi' >> ~/.zshrc
 echo "# END: Setup by setup_local_home.sh" >> ~/.zshrc
 
 # Setup profile
 echo "# BEGIN: Setup by setup_local_home.sh" >> ~/.profile
-echo 'if [ -f "$ZHANGYI_ENV_DIR/shell-config/profile.sh" ]; then' >> ~/.profile
-echo '    . "$ZHANGYI_ENV_DIR/shell-config/profile.sh"' >> ~/.profile
+echo 'if [ -f "$ZHANGYI_ENV_DIR/'"${MODULE_NAME}"'/profile.sh" ]; then' >> ~/.profile
+echo '    . "$ZHANGYI_ENV_DIR/'"${MODULE_NAME}"'/profile.sh"' >> ~/.profile
 echo 'fi' >> ~/.profile
 echo "# END: Setup by setup_local_home.sh" >> ~/.profile
 
 # Setup emacs
 echo ";; BEGIN: Setup by setup_local_home.sh" >> ~/.emacs
-echo "(load-file \"$ENV_DIR/emacs-config/start.el\")" >> ~/.emacs
+echo "(load-file \"$ENV_DIR/${MODULE_NAME}/emacs-config/start.el\")" >> ~/.emacs
 echo ";; END: Setup by setup_local_home.sh" >> ~/.emacs
 
 # Setup screen
-if [ -f "$CONFIG_DIR/shell-config/screenrc" ]; then
-    ln -sf "$CONFIG_DIR/shell-config/screenrc" ~/.screenrc
+if [ -f "$CONFIG_DIR/${MODULE_NAME}/screenrc" ]; then
+    ln -sf "$CONFIG_DIR/${MODULE_NAME}/screenrc" ~/.screenrc
 fi
 
 # Setup git
-if [ -f "$CONFIG_DIR/shell-config/gitignore" ]; then
-    ln -sf "$CONFIG_DIR/shell-config/gitignore" ~/.gitignore
+if [ -f "$CONFIG_DIR/${MODULE_NAME}/git/gitignore" ]; then
+    ln -sf "$CONFIG_DIR/${MODULE_NAME}/git/gitignore" ~/git/.gitignore
+fi
+
+if [ -f "$CONFIG_DIR/${MODULE_NAME}/git/gitconfig" ]; then
+    ln -sf "$CONFIG_DIR/${MODULE_NAME}/git/gitconfig" ~/git/.gitconfig
 fi
 
 # Setup environment related stuff
 if [ "$ENV_DIR" != "$CONFIG_DIR" ]; then
-    if [ -f "$ENV_DIR/shell-config/sbin/setup_local_home.sh" ]; then
-        . "$ENV_DIR/shell-config/sbin/setup_local_home.sh"
+    if [ -f "$ENV_DIR/${MODULE_NAME}/sbin/setup_local_home.sh" ]; then
+        . "$ENV_DIR/${MODULE_NAME}/sbin/setup_local_home.sh"
     fi
 fi
