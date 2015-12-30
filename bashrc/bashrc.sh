da32878f6a1e149f46515750bf5ebbf5680cba8c
Yi Zhang <starain.zhang@gmail.com>
2015-12-29 22:05:21 -0800

    Add LANG export

--- bashrc/bashrc.sh
+++ bashrc/bashrc.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+export EDITOR='emacsclient -a ""'
+export TERM=xterm-256color
+export LANG=en_US.UTF-8
+
 if [ -f "$ZHANGYI_CONFIG_DIR/shell-config/bashrc/alias.sh" ]; then
     . "$ZHANGYI_CONFIG_DIR/shell-config/bashrc/alias.sh"
 fi
 
-export EDITOR='emacsclient -a ""'
-export TERM=xterm-256color
