11fa665864b5c56374a26050155215ff0c50d29b
Yi Zhang <starain.zhang@gmail.com>
2015-12-29 22:01:45 -0800

    Remove unused alias.

--- bashrc/alias.sh
+++ bashrc/alias.sh
@@ -4,9 +4,4 @@ alias rs='screen -h 5000 -x yiz || screen -h 5000 -S yiz'
 alias ls='ls --color=auto'
 alias l='ls'
 alias e='emacsclient -a ""'
-alias g='git'
-alias gcm='git commit -a -m'
-alias gl='git log'
-alias gst='git status'
 alias es="emacs -nw --daemon"
-
