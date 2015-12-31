bed937a13d0d6d14982369351e071202022d3ee6
Yi Zhang <starain.zhang@gmail.com>
2015-12-30 21:48:47 -0800

    Update setup script not to worry about env setups. Env setup should be taking care of by env setup script.

--- setup/setup_local_home.sh
+++ setup/setup_local_home.sh
@@ -1,76 +1,84 @@
 #!/bin/bash
 
 # Setup environment
-if [ $# -lt 0 ]
-then
-    echo "Usage ${BASH_SOURCE[0]} [environment_sub_dir]"
-    exit
-fi
+# if [ $# -gt 1 ]
+# then
+#     echo "Usage setup_local_home.sh [config_dir]"
+#     echo "Default config directory is $HOME/.config.zhangyi"
+#     exit
+# fi
 
-CONFIG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
-ENV_DIR="$CONFIG_DIR"
-if [ $# -eq 1 ]
-then
-    ENV_DIR_NAME="$1"
-    ENV_DIR="$CONFIG_DIR/$ENV_DIR_NAME"
-fi
+CONFIG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-MODULE_NAME="shell-config"
-cd $CONFIG_DIR/$MODULE_NAME
-git submodule update --init --recursive
+# Checkout and update all submodules
 cd $CONFIG_DIR
+git submodule update --init --recursive
 
 MAGIC_ANCHOR="Setup by setup_local_home.sh"
 
 # Setup bashrc
+echo 'Setting up bashrc'
+
 echo "# BEGIN: $MAGIC_ANCHOR" >> ~/.bashrc
 echo "export ZHANGYI_CONFIG_DIR=\"$CONFIG_DIR\"" >> ~/.bashrc
-echo "export ZHANGYI_ENV_DIR=\"$ENV_DIR\"" >> ~/.bashrc
+if [ -f "$CONFIG_DIR/bashrc/bashrc.sh" ]; then
+    echo 'Adding bashrc config'
 
-echo 'if [ -f "$ZHANGYI_ENV_DIR/'"$MODULE_NAME"'/bashrc/bashrc.sh" ]; then' >> ~/.bashrc
-echo '    . "$ZHANGYI_ENV_DIR/'"$MODULE_NAME"'/bashrc/bashrc.sh"' >> ~/.bashrc
-echo 'fi' >> ~/.bashrc
+    echo '. "$ZHANGYI_CONFIG_DIR/bashrc/bashrc.sh"' >> ~/.bashrc
+fi
 echo "# END: $MAGIC_ANCHOR" >> ~/.bashrc
 
 # Setup zshrc
+echo 'Setting up zshrc'
+
 echo "# BEGIN: $MAGIC_ANCHOR" >> ~/.zshrc
 echo "export ZHANGYI_CONFIG_DIR=\"$CONFIG_DIR\"" >> ~/.zshrc
-echo "export ZHANGYI_ENV_DIR=\"$ENV_DIR\"" >> ~/.zshrc
+if [ -f "$CONFIG_DIR/zshrc/zshrc" ]; then
+    echo 'Adding zshrc config'
 
-echo 'if [ -f "$ZHANGYI_ENV_DIR/'"$MODULE_NAME"'/zshrc/zshrc" ]; then' >> ~/.zshrc
-echo '    . "$ZHANGYI_ENV_DIR/'"$MODULE_NAME"'/zshrc/zshrc"' >> ~/.zshrc
-echo 'fi' >> ~/.zshrc
+    echo '. "$ZHANGYI_CONFIG_DIR/zshrc/zshrc"' >> ~/.zshrc
+fi
 echo "# END: $MAGIC_ANCHOR" >> ~/.zshrc
 
 # Setup profile
-echo "# BEGIN: $MAGIC_ANCHOR" >> ~/.profile
-echo 'if [ -f "$ZHANGYI_ENV_DIR/'"$MODULE_NAME"'/profile.sh" ]; then' >> ~/.profile
-echo '    . "$ZHANGYI_ENV_DIR/'"$MODULE_NAME"'/profile.sh"' >> ~/.profile
-echo 'fi' >> ~/.profile
-echo "# END: $MAGIC_ANCHOR" >> ~/.profile
+if [ -f "$CONFIG_DIR/profile.sh" ]; then
+    echo 'Setting up profile'
+
+    echo "# BEGIN: $MAGIC_ANCHOR" >> ~/.profile
+    echo '. "$ZHANGYI_CONFIG_DIR/profile.sh"' >> ~/.profile
+    echo "# END: $MAGIC_ANCHOR" >> ~/.profile
+fi
 
 # Setup screen
-if [ -f "$CONFIG_DIR/$MODULE_NAME/screenrc" ]; then
-    ln -sf "$CONFIG_DIR/$MODULE_NAME/screenrc" ~/.screenrc
+if [ -f "$CONFIG_DIR/screenrc" ]; then
+    echo 'Setting up screenrc'
+
+    ln -sf "$CONFIG_DIR/screenrc" ~/.screenrc
 fi
 
 # Setup git
-if [ -f "$CONFIG_DIR/$MODULE_NAME/git/gitignore" ]; then
-    ln -sf "$CONFIG_DIR/$MODULE_NAME/git/gitignore" ~/.gitignore
+if [ -f "$CONFIG_DIR/git/gitignore" ]; then
+    echo 'Setting up gitignore'
+
+    ln -sf "$CONFIG_DIR/git/gitignore" ~/.gitignore
 fi
 
-if [ -f "$CONFIG_DIR/$MODULE_NAME/git/gitconfig" ]; then
-    ln -sf "$CONFIG_DIR/$MODULE_NAME/git/gitconfig" ~/.gitconfig
+if [ -f "$CONFIG_DIR/git/gitconfig" ]; then
+    echo 'Setting up gitconfig'
+
+    ln -sf "$CONFIG_DIR/git/gitconfig" ~/.gitconfig
 fi
 
 # Setup emacs
-if [ -f "$CONFIG_DIR/$MODULE_NAME/emacs-config/setup/setup_local_home.sh" ]; then
-    . "$CONFIG_DIR/$MODULE_NAME/emacs-config/setup/setup_local_home.sh"
+if [ -f "$CONFIG_DIR/emacs-config/setup/setup_local_home.sh" ]; then
+    echo 'Setting up emacs'
+
+    . "$CONFIG_DIR/emacs-config/setup/setup_local_home.sh"
 fi
 
 # Setup environment related stuff
-if [ "$ENV_DIR" != "$CONFIG_DIR" ]; then
-    if [ -f "$ENV_DIR/$MODULE_NAME/sbin/setup_local_home.sh" ]; then
-        . "$ENV_DIR/$MODULE_NAME/sbin/setup_local_home.sh"
-    fi
-fi
+# if [ "$ENV_DIR" != "$CONFIG_DIR" ]; then
+#     if [ -f "$ENV_DIR/$MODULE_NAME/sbin/setup_local_home.sh" ]; then
+#         . "$ENV_DIR/$MODULE_NAME/sbin/setup_local_home.sh"
+#     fi
+# fi
