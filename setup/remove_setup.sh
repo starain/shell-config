257395bfd4d3ed210efe57e7c1608c71c1035224
Yi Zhang <starain.zhang@gmail.com>
2015-12-30 22:49:59 -0800

    setup_local_home.sh: Better modularity with for loop, safer ln command without -f, remove emacs-config setup. remove_setup.sh: Remove env dir related setup, better modularity with for loop, safer removing method by using find, remove emacs-config related setup.

--- setup/remove_setup.sh
+++ setup/remove_setup.sh
@@ -1,38 +1,20 @@
 #!/bin/bash
 
-# Setup environment
-if [ $# -lt 0 ]
-then
-    echo "Usage ${BASH_SOURCE[0]} [environment_sub_dir]"
-    exit
-fi
-
-CONFIG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
-ENV_DIR="$CONFIG_DIR"
-if [ $# -eq 1 ]
-then
-    ENV_DIR_NAME="$1"
-    ENV_DIR="$CONFIG_DIR/$ENV_DIR_NAME"
-fi
-
-MODULE_NAME="shell-config"
+CONFIG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 MAGIC_ANCHOR="Setup by setup_local_home.sh"
 
 TMP_FILE=$(mktemp)
 
-for f in "$HOME/.bashrc" "$HOME/.zshrc" "$HOME/.profile" "$HOME/.emacs"
+for f in "$HOME/.bashrc" "$HOME/.zshrc" "$HOME/.profile"
 do
+    echo "Removing config from $f"
     sed "/$MAGIC_ANCHOR/,/$MAGIC_ANCHOR/d" $f > $TMP_FILE && mv $TMP_FILE $f
 done
 
-if [ -f "$CONFIG_DIR/${MODULE_NAME}/screenrc" ]; then
-    rm ~/.screenrc
-fi
-
-if [ -f "$CONFIG_DIR/${MODULE_NAME}/git/gitignore" ]; then
-    rm ~/.gitignore
-fi
-
-if [ -f "$CONFIG_DIR/${MODULE_NAME}/git/gitconfig" ]; then
-    rm ~/.gitconfig
-fi
+for f in "screenrc" "gitignore" "gitconfig"
+do
+    if [ -f "$CONFIG_DIR/$f" ]; then
+        echo "Removing file $f"
+        find ~/.$f -type l -delete
+    fi
+done
--- setup/setup_local_home.sh
+++ setup/setup_local_home.sh
@@ -17,64 +17,36 @@ git submodule update --init --recursive
 MAGIC_ANCHOR="Setup by setup_local_home.sh"
 
 # Setup bashrc
-echo 'Setting up bashrc'
+for f in "bashrc" "zshrc"
+do
+    echo "Setting up $f"
 
-echo "# BEGIN: $MAGIC_ANCHOR" >> ~/.bashrc
-echo "export ZHANGYI_CONFIG_DIR=\"$CONFIG_DIR\"" >> ~/.bashrc
-if [ -f "$CONFIG_DIR/bashrc/bashrc.sh" ]; then
-    echo 'Adding bashrc config'
-
-    echo '. "$ZHANGYI_CONFIG_DIR/bashrc/bashrc.sh"' >> ~/.bashrc
-fi
-echo "# END: $MAGIC_ANCHOR" >> ~/.bashrc
-
-# Setup zshrc
-echo 'Setting up zshrc'
-
-echo "# BEGIN: $MAGIC_ANCHOR" >> ~/.zshrc
-echo "export ZHANGYI_CONFIG_DIR=\"$CONFIG_DIR\"" >> ~/.zshrc
-if [ -f "$CONFIG_DIR/zshrc/zshrc" ]; then
-    echo 'Adding zshrc config'
-
-    echo '. "$ZHANGYI_CONFIG_DIR/zshrc/zshrc"' >> ~/.zshrc
-fi
-echo "# END: $MAGIC_ANCHOR" >> ~/.zshrc
+    echo "# BEGIN: $MAGIC_ANCHOR" >> ~/.$f
+    echo "export ZHANGYI_CONFIG_DIR=\"$CONFIG_DIR\"" >> ~/.$f
+    if [ -f "$CONFIG_DIR/$f/$f" ]; then
+        echo "Adding $f config"
+        echo '. "$ZHANGYI_CONFIG_DIR/$f/$f"' >> ~/.$f
+    fi
+    echo "# END: $MAGIC_ANCHOR" >> ~/.$f
+done
 
 # Setup profile
-if [ -f "$CONFIG_DIR/profile.sh" ]; then
+if [ -f "$CONFIG_DIR/profile" ]; then
     echo 'Setting up profile'
 
     echo "# BEGIN: $MAGIC_ANCHOR" >> ~/.profile
-    echo '. "$ZHANGYI_CONFIG_DIR/profile.sh"' >> ~/.profile
+    echo '. "$ZHANGYI_CONFIG_DIR/profile"' >> ~/.profile
     echo "# END: $MAGIC_ANCHOR" >> ~/.profile
 fi
 
-# Setup screen
-if [ -f "$CONFIG_DIR/screenrc" ]; then
-    echo 'Setting up screenrc'
+for f in "screenrc" "gitignore" "gitconfig"
+do
+    if [ -f "$CONFIG_DIR/$f" ]; then
+        echo "Setting up $f"
 
-    ln -sf "$CONFIG_DIR/screenrc" ~/.screenrc
-fi
-
-# Setup git
-if [ -f "$CONFIG_DIR/git/gitignore" ]; then
-    echo 'Setting up gitignore'
-
-    ln -sf "$CONFIG_DIR/git/gitignore" ~/.gitignore
-fi
-
-if [ -f "$CONFIG_DIR/git/gitconfig" ]; then
-    echo 'Setting up gitconfig'
-
-    ln -sf "$CONFIG_DIR/git/gitconfig" ~/.gitconfig
-fi
-
-# Setup emacs
-if [ -f "$CONFIG_DIR/emacs-config/setup/setup_local_home.sh" ]; then
-    echo 'Setting up emacs'
-
-    . "$CONFIG_DIR/emacs-config/setup/setup_local_home.sh"
-fi
+        ln -s "$CONFIG_DIR/$f" ~/.$f
+    fi
+done
 
 # Setup environment related stuff
 # if [ "$ENV_DIR" != "$CONFIG_DIR" ]; then
