ffcf2ffa17d593cc73255bd4c220a1050b1f09a7
Yi Zhang <starain.zhang@gmail.com>
2016-01-01 20:49:21 -0800

    Limit xterm-256 to emacs only, urxvt depends on the right term parameter to work with zsh partial history matching.

--- bashrc/alias.sh
+++ bashrc/alias.sh
@@ -3,5 +3,7 @@
 alias rs='screen -h 5000 -x yiz || screen -h 5000 -S yiz'
 alias ls='ls --color=auto'
 alias l='ls'
+alias emacsclient='TERM=xterm-256color emacsclient'
+alias emacs='TERM=xterm-256color emacs'
 alias e='emacsclient -a ""'
 alias es="emacs -nw --daemon"
