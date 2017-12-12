b461d1cc43e809f4c54f098f27dbceddc3b6f7dc
Yi Zhang <starain.zhang@gmail.com>
2017-12-11 22:53:56 -0800

    Some tweak for alias. Prepare to have a way to customize oh my zsh before init

--- bashrc/alias.sh
+++ bashrc/alias.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-alias rs='screen -h 5000 -x yiz || screen -h 5000 -S yiz'
+alias tw='tmux new -s work || tmux a -t work'
 alias ls='ls --color=auto'
 alias l='ls'
 alias emacsclient='TERM=xterm-256color emacsclient'
 alias emacs='TERM=xterm-256color emacs'
-alias e='emacsclient -a ""'
+alias e='emacsclient -t'
 alias es="emacs -nw --daemon"
